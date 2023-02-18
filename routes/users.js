275f1d6a89cf7627b4fe5e59ae7e3762cccb151c
Zeeshnhmd <zeeshnhmd.1@gmail.com>
2023-02-18 17:18:58 +0530

    feat: routes

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,9 @@ const express = require('express');
 const routes = express.Router();
 
 //Login page
-routes.get('/login', (req, res) => res.send('Login'));
+routes.get('/login', (req, res) => res.render('login'));
 
 //Register page
-routes.get('/register', (req, res) => res.send('Register'));
+routes.get('/register', (req, res) => res.render('register'));
 
 module.exports = routes;
